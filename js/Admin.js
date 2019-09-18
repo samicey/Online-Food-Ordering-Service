6bd6a95e0b513d7f41e3e3b1f387176061aec4b0
Obikaonu Samuel Chibuike <samuelchibuike22@gmail.com>
2019-09-18 10:29:15 +0100

    Add scripts that fades section In and Out according to the link clicked

--- js/Admin.js
+++ js/Admin.js
@@ -1,5 +1,37 @@
 $(document).ready(function (){
-   $('#AddProduct') .click(function (e){
-       
+   $('#ProductOrder').hide();
+   $('#viewAllProductsPage').hide();
+   $('.viewOrders').hide();
+   $('#AdminHome').click(function(e){
+       e.preventDefault();
+       $('#welcomeMessage').fadeIn();
+       $('#ProductOrder').fadeOut();
+       $('#viewAllProductsPage').fadeOut();
+       $('.viewOrders').fadeOut();
    })
+   $('#AddProduct').click(function(e){
+    e.preventDefault();
+    $('#welcomeMessage').fadeOut();
+    $('#ProductOrder').fadeIn();
+    $('#viewAllProductsPage').fadeOut();
+    $('.viewOrders').fadeOut();
+})
+$('#viewAllProducts').click(function(e){
+    e.preventDefault();
+    $('#welcomeMessage').fadeOut();
+    $('#ProductOrder').fadeOut();
+    $('#viewAllProductsPage').fadeIn();
+    $('.viewOrders').fadeOut();
+})
+$('#viewOrders').click(function(e){
+    e.preventDefault();
+    $('#welcomeMessage').fadeOut();
+    $('#ProductOrder').fadeOut();
+    $('#viewAllProductsPage').fadeOut();
+    $('.viewOrders').fadeIn();
+})
+$('#AddGoods').click(function (e){
+    e.preventDefault();
+    const 
+})
 })
