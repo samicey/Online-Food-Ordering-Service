19be8130dbde8368ba3b962672f90fbeacbb62ff
Obikaonu Samuel Chibuike <samuelchibuike22@gmail.com>
2019-09-19 23:36:22 +0100

    IMplemented the view and order functionality

--- Public/js/products.js
+++ Public/js/products.js
@@ -14,24 +14,74 @@
               <span><span>&#8358;</span>${product.price}</span><br />
               <p>${product.Description}</p>
               <p>${product.quantity} avaliable</p></strong>
-              <button class="btn btn-sm btn-primary orderbtn" data-orderid="${product.id}" >View</button>
+              <button class="btn btn-sm btn-primary viewbtn" data-viewid="${product.id}" >View</button>
           </span>`
              ))
-             const  view = $('.orderbtn');
+             const  view = $('.viewbtn');
              view.on('click', function(even){
                  even.preventDefault()
+
                  $("#Allproduct").fadeOut()
-                 const editid = even.currentTarget.dataset.orderid
+                 var viewid = even.currentTarget.dataset.viewid
                  $.ajax({
                     method: 'GET',
-                    url:`http://localhost:3000/products/${editid}`,
-                    success: function (){
-                         $(`#${deleteID}`).remove()
+                    url:`http://localhost:3000/products/${viewid}`,
+                    success: function (product){
+                        
+                         
+                             $('#OneOrder').append(
+                                `<span>
+              
+              <div><strong><a href="${product.Image}"><img src="${product.Image}" alt="food image"/></div></a>
+	    <h4>${product.ProductName}</h4>
+              <span><span>&#8358;</span>${product.price}</span><br />
+              <p>${product.Description}</p>
+              <p>${product.quantity} avaliable</p></strong>
+              <button class="btn btn-sm btn-primary orderbtn" data-orderid="${product.id}" >Order</button>
+          </span>` 
+                             )
+
+                             const  order = $('.orderbtn');
+                order.on('click',function(){
+                    
+                 $.ajax({
+                    method: 'GET',
+                    url:`http://localhost:3000/products/${viewid}`,
+                    
+                    success: function (product){
+                        
+                        const   ProductName = product.ProductName,
+                                category = product.category,
+                                Description = product.Description,
+                                price = product.price;
+                                
+                            console.log(ProductName)
+                         
+                            $.ajax({
+                                method: 'POST',
+                                url:`http://localhost:3000/orders`,
+                                data:{
+                                    ProductName,
+                                    category,
+                                    Description,
+                                    price
+                                },
+                                success: function (){
+                                    $('.orderbtn').text('Ordered')
+                                }               
+                            })
+                         
                     }
                 })
+                     
+                })
+                         
+                    }
+                })
+                
 
              })
-        },
+        }
         
     });
 })
